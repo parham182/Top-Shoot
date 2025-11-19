fb337585f0f50edd26519712df359fe471799acf
parham <parhamqop@gmail.com>
2025-11-19 18:17:37 +0330

    Add Game

---
+++ Top Shoot/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;            // پریفب تیر
+    public Transform playerTransform;
+    public Transform playerTransform2;          // محل شروع تیر از روی پلیر
+    public float bulletSpeed = 10f;             // سرعت تیر
+    public float fireRate = 0.2f;               // فاصله زمانی بین هر شلیک (ثانیه)
+
+    private bool isShooting = false;             // آیا دکمه نگه داشته شده؟
+    private float nextFireTime = 0f;             // زمان بعدی که اجازه شلیک داریم
+
+    // این متد را از Event دکمه UI در OnPointerDown فراخوانی کنید
+    public void StartShooting()
+    {
+        isShooting = true;
+    }
+
+    // این متد را از Event دکمه UI در OnPointerUp فراخوانی کنید
+    public void StopShooting()
+    {
+        isShooting = false;
+    }
+
+    void Update()
+    {
+        if (isShooting && Time.time >= nextFireTime)
+        {
+            Shoot();
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, playerTransform.position, bulletPrefab.transform.rotation);
+        GameObject bullet2 = Instantiate(bulletPrefab, playerTransform2.position, bulletPrefab.transform.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        Rigidbody2D rb1 = bullet2.GetComponent<Rigidbody2D>();
+        if (rb != null && rb1 != null)
+        {
+            rb.linearVelocity = Vector2.right * bulletSpeed;
+            rb1.linearVelocity = Vector2.right * bulletSpeed; // تیر به جلو پرتاب می‌شود
+             // تیر به جلو پرتاب می‌شود
+        }
+    }
+}
